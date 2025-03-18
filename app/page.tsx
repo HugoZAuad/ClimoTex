3d5a99d32a92423c5e8090a762ce41341f597613
HugoZAuad <hugozeymer@hotmail.com>
2025-03-18 19:13:26 -0300

    Feito alterações no CSS, adicionado a transição quando o usuario escolhe outra cidade, e importação das logos e icone de barra do navegador

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { useState } from 'react';
+import { motion, AnimatePresence } from 'framer-motion'; 
 import Humidity from './Components/Humidity/Humidity';
 import WindSpeed from './Components/Wind/Wind';
 import CityInput from './Components/CityInput/CityInput';
 import Temperature from './Components/Temperature/Temperature';
 import Condition from './Components/Condition/Condition';
+import Image from 'next/image';
+import ClimoLogo from '../public/CLIMO_logo.png';
 
 interface WeatherData {
   weather: { description: string }[];
@@ -41,23 +44,35 @@ export default function Home() {
 
   return (
     <div className="p-4 m-2 text-background">
-      <h1 className="text-3xl font-bold">ClimoTex</h1> {/* Inserir depois a foto da logo */}
-      <p>O seu aplicativo de clima em tempo real</p>
+      {/* Adicionando o logo */}
+      <div className="flex mb-4">
+        <Image src={ClimoLogo} alt="ClimoTex Logo" width={250} height={250} />
+      </div>
+
+      <p className="text-lg font-semibold">Busque pela cidade desejada</p>
       <CityInput onCityChange={setCity} onSearch={handleSearch} />
-      {error && <p className="text-red-500">{error}</p>}
-      {weather && (
-        <div>
-          <h2 className="text-xl font-semibold flex justify-center items-center mt-15 mb-35 gap-1">
-            Em <span className='text-dodger font-bold'>{weather.name}</span> está com as seguintes condições climáticas:
-          </h2>
-          <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4'>
-            <Temperature value={weather.main.temp} />
-            <Condition description={weather.weather[0].description} />
-            <Humidity value={weather.main.humidity} />
-            <WindSpeed value={weather.wind.speed} />
-          </div>
-        </div>
-      )}
+      {error && <p className="text-red-500 text-center mt-4">{error}</p>}
+      <AnimatePresence>
+        {weather && (
+          <motion.div
+            key={weather.name}
+            initial={{ opacity: 0, y: 20 }} 
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -20 }} 
+            transition={{ duration: 0.5 }} 
+          >
+            <h2 className="text-xl font-semibold flex justify-center items-center mt-8 mb-8 gap-1">
+              Em <span className="text-dodger font-bold">{weather.name}</span> está com as seguintes condições climáticas:
+            </h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+              <Temperature value={weather.main.temp} />
+              <Condition description={weather.weather[0].description} />
+              <Humidity value={weather.main.humidity} />
+              <WindSpeed value={weather.wind.speed} />
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
